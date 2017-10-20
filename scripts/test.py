c71317ab9f8697dcb8fbecf3acd6f52cd165c4bd
andrewww2 <andrwww@utexas.edu>
2017-10-19 23:22:46 -0400

    initial commit

---
+++ scripts/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+#from std_msgs.msg import String
+from phidget_stepper.msg import StepperConfig
+def talker():
+    pub = rospy.Publisher("motor_config", StepperConfig, queue_size=10)
+    rospy.init_node('motor_test', anonymous=True)
+    rate = rospy.Rate(1) # 2hz
+
+    while not rospy.is_shutdown():
+        #hello_str = "hello world %s" % rospy.get_time()
+        #rospy.loginfo(hello_str)
+        leftCmd = StepperConfig()
+        leftCmd.toLeft = True
+        leftCmd.engage = True #True for sending  power to motor coils (make it move)
+        leftCmd.mode = True #True
+        leftCmd.velocityLimit =  float(20)
+        leftCmd.acceleration = float(100)
+        leftCmd.position = int(1500)
+
+        print("sent it boss")
+        pub.publish(leftCmd)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
